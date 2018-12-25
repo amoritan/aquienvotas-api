9d9802f8033c0ce8bf3df4ecd5c05d929e299d68
Andres Moritan <amoritan@me.com>
2018-12-25 13:37:12 -0300

    Provinces and cities seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+provinces = HTTP.get("https://apis.datos.gob.ar/georef/api/provincias").parse
+provinces["provincias"].each do |province|
+  _province = Province.create(name: province["nombre"], origin_id: province["id"])
+  puts "Province #{province["nombre"]} was created"
+  cities = HTTP.get("https://apis.datos.gob.ar/georef/api/departamentos?provincia=#{province["id"]}&max=1000").parse
+  cities["departamentos"].each do |city|
+    City.create(name: city["nombre"], origin_id: city["id"], province: _province)
+    puts "City #{city["nombre"]} was created at province #{province["nombre"]}"
+  end
+end
