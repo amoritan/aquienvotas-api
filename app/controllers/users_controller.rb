b472c1a2e60ec8da1baf96aed41c1ceeae57645e
Andres Moritan <amoritan@me.com>
2019-02-11 23:37:55 -0300

    309c9c8 minor fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,12 @@ class UsersController < ApplicationController
     @users = User.all
     authorize @users
 
-    @response = {}
+    @response = []
 
     Province.joins(:locations).each do |province|
       province.locations.each_with_index { |location, index|
-        @response[province.locations.count > 1 ? "#{province.code}-#{index + 1}" : province.code] = {
+        @response << {
+          :code => province.locations.count > 1 ? "#{province.code}-#{index + 1}" : province.code,
           :name => location.name,
           :total => (@users.where(location: location).count.to_f * 100 / @users.count.to_f).round(2)
         }
