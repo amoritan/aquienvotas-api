9cc27281193e4e649fe3a299f218ff65369a67c8
Andres Moritan <amoritan@me.com>
2019-02-15 15:35:54 -0300

    Avatar added to candidates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -5,5 +5,11 @@ class Candidate < ApplicationRecord
   has_and_belongs_to_many :ballots
   has_many :votes, as: :choice
 
+  has_one_attached :avatar
+
+  def avatar_url
+    self.avatar.attached? ? Rails.application.routes.url_helpers.url_for(self.avatar.variant(combine_options: {resize: "480x480", gravity: "center", extent: "480x480", background: "white"})) : nil
+  end
+
   attr_accessor :result
 end
--- app/serializers/candidate_serializer.rb
+++ app/serializers/candidate_serializer.rb
@@ -1,4 +1,5 @@
 class CandidateSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :color, :result
+  attribute :avatar_url, key: :avatar
   belongs_to :party
 end
