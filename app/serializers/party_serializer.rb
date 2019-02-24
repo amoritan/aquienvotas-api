1c73114a9a04f5eb393b90a55bb4570ab667e26d
Andres Moritan <amoritan@me.com>
2019-02-24 18:40:28 -0300

    Minor serializer fix

--- app/serializers/party_serializer.rb
+++ app/serializers/party_serializer.rb
@@ -1,6 +1,6 @@
 class PartySerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :color, :result
-  attribute :candidates do
+  attribute :candidates, if: -> { object.result } do
     object.result_candidates.map { |candidate| CandidateSerializer.new(candidate) }.sort_by { |candidate| candidate.object.result }.reverse
-  end 
+  end
 end
