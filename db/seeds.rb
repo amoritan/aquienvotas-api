6eba658973599bd88e732a0ac476fe5bb3f550cb
Andres Moritan <amoritan@me.com>
2019-04-10 20:59:40 -0300

    GNU AGPL license notice added to files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,26 @@
+########################################################################################
+########################################################################################
+####                                                                                ####
+####  This file is part of AQuienVotas.                                             ####
+####                                                                                ####
+####  AQuienVotas is free software: you can redistribute it and/or modify           ####
+####  it under the terms of the GNU Affero General Public License as published by   ####
+####  the Free Software Foundation, either version 3 of the License, or             ####
+####  (at your option) any later version.                                           ####
+####                                                                                ####
+####  AQuienVotas is distributed in the hope that it will be useful,                ####
+####  but WITHOUT ANY WARRANTY; without even the implied warranty of                ####
+####  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 ####
+####  GNU Affero General Public License for more details.                           ####
+####                                                                                ####
+####  You should have received a copy of the GNU Affero General Public License      ####
+####  along with AQuienVotas.  If not, see <https://www.gnu.org/licenses/>.         ####
+####                                                                                ####
+########################################################################################
+########################################################################################
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
